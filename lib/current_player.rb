5f4acb17f40f7fd47946d2cc65995806e393c9b9
Peter Sampson <petersampson010@gmail.com>
2019-07-10 07:06:54 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,11 @@ def turn_count(board)
 end 
 
 def current_player(board)
-  return ((counter % 2 == 0) ? "X" : "O") 
+  if turn_count(board) % 2 == 0 
+    return "X"
+  else 
+    return "O"
+  end 
 end 
 
   
